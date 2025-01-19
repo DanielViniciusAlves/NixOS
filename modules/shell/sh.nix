17db2edc732a9e1c3c72949113118c10e6897cef
DanielViniciusAlves <danielribeiroalves0@gmail.com>
2025-01-19 10:44:53 -0300

    Add zsh promt config

--- modules/shell/sh.nix
+++ modules/shell/sh.nix
@@ -4,21 +4,42 @@ let
   myAliases = { };
 in
 {
+  home.packages = with pkgs; [
+    fzf
+  ];
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    plugins = [
+      {
+        name = "zsh-git-prompt";
+        src = pkgs.zsh-git-prompt;
+        file = "share/zsh-git-prompt/zshrc.sh";
+      }
+    ];
 
     oh-my-zsh = {
       enable = true;
-      theme = "agnoster";
-      plugins = [ "git" ];
+      plugins = [ "" ];
     };
+
+    initExtra = ''
+      PROMPT="%U%F{33}%n%f%u@%U%F{4}%m%f%u %F{6}✘  %F{195}%~%f
+      %F{33}→%f "
+
+      RPROMPT='$(git_super_status)'
+
+      bindkey '^P' history-beginning-search-backward
+      bindkey '^N' history-beginning-search-forward
+    '';
+
   };
 
-  home.packages = with pkgs; [
-    fzf
-  ];
+  home.file = { };
 
   programs.fzf = {
     enable = true;
